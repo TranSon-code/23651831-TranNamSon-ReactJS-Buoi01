d1c2a4a7f766c15c7078d049a10d2d9b00050f4f
TranSone-Code <23651831.son@student.iuh.edu.vn>
2026-01-18 18:40:31 +0700

    Hoàn thành bài 2

---
+++ react_css&jsx/bai-02/src/App.jsx
@@ -0,0 +1,13 @@
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <div className="app">
+      <Button type="primary">Primary</Button>
+      <Button type="success">Success</Button>
+      <Button type="danger">Danger</Button>
+    </div>
+  );
+}
+
+export default App;
